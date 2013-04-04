3533b44d80aa69468764fe939c4575a2bbb24922
mohammed sadiq <sadiqmmm@gmail.com>
2013-04-04 20:02:03 +0530

    Created Rails app and added scaffold status

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gobook::Application.config.secret_token = '11972f9fcbaac48744e41723e6b0a30f09d586d9a8ae76503329251b865b81732e5b25704b329e7cc6e877fe7c0a31b817b8ae8a4aeb4df8f004e9b81ad069ec'
